0ef8cc52621dc3f9e265156f719f7ff0cc22061d
Luannguyencg <luannguyen12069@gmail.com>
2022-04-02 03:51:06 +0700

    up

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -140,8 +140,21 @@ function handleMessage(sender_psid, received_message) {
 
 // Handles messaging_postbacks events
 function handlePostback(sender_psid, received_postback) {
-    // Construct the message body
+    let response;
 
+    // Get the payload for the postback
+    let payload = received_postback.payload;
+
+    // Set the response based on the postback payload
+    if (payload === 'yes') {
+        response = { "text": "Thanks!" }
+    } else if (payload === 'no') {
+        response = { "text": "Oops, try sending another image." }
+    }else if(payload === 'GET_STARTED'){
+        response = { "text": "ok. xin chào mừng bạn abc đến với page của Luân"}
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response);
 }
 
 // Sends response messages via the Send API
