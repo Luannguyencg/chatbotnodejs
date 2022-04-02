d0f4a1f5f57d32042bfd1cbb5d0f2c89fe8fa455
Luannguyencg <luannguyen12069@gmail.com>
2022-04-02 16:48:23 +0700

    upff

--- src/services/chatbotService.js
+++ src/services/chatbotService.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 import request from "request";
-
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN
 const IMAGE_GET_STARTED = 'https://bit.ly/luan-botchat1'
 
 // Handles messages events
